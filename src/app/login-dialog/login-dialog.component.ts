c53e746a2575ce2ec3fc9f75614e92cff32cfc16
Maxime <gendron.maxime1@gmail.com>
2020-06-28 23:50:29 -0400

    Changed to reactive forms

--- src/app/login-dialog/login-dialog.component.ts
+++ src/app/login-dialog/login-dialog.component.ts
@@ -4,7 +4,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { Subscription } from 'rxjs';
 import { UserService } from '../services/user.service';
 import { AuthService } from '../services/auth.service';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login-dialog',
@@ -14,27 +14,30 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class LoginDialogComponent implements OnDestroy {
 
   private subscription: Subscription = new Subscription();
-  username : string;
-  password : string;
   error : string;
   loading = false;
 
+  loginForm = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
   constructor(public dialogRef: MatDialogRef<LoginDialogComponent>, private translateService: TranslateService,
-    private userService: UserService, private authService: AuthService) {}
+    private userService: UserService, private authService: AuthService, private fb: FormBuilder) {}
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
 
   isDisabled(): boolean {
-    return !this.username || !this.password || this.loading;
+    return !this.loginForm.valid || this.loading;
   }
 
   //Try to login the user
   login(): void {
     this.loading = true;
 
-    this.subscription.add(this.userService.validateUser(this.username, this.password).subscribe(response => {
+    this.subscription.add(this.userService.validateUser(this.loginForm.value.username, this.loginForm.value.password).subscribe(response => {
       //Look if the user is valid or not
       if (response.isValid !== true) {
         this.error = this.translateService.instant('error.userNotfound');
