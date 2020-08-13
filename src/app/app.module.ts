5deeacd6732958b5e2089e134748572679e2ec63
Maxime <gendron.maxime1@gmail.com>
2020-08-12 22:29:25 -0400

    Added models & users folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,75 +1,22 @@
 import { AppComponent } from './app.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { RouterModule, Routes } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { JwtInterceptor } from './interceptor/jwt.interceptor';
 import { ErrorInterceptor } from './interceptor/error.interceptor';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatIconModule } from '@angular/material/icon';
 import { IndexComponent } from './index/index.component';
-import { AuthGuard } from './services/auth.guard';
 import { NavbarComponent } from './navbar/navbar.component'
-import { MatMenuModule } from '@angular/material/menu';
-import { LoginDialogComponent } from './login-dialog/login-dialog.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { RegisterPopupComponent } from './register-popup/register-popup.component';
-
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
-}
-
-const appRoutes: Routes = [
-  { path: '', component: IndexComponent },
-  // { path: 'index', component: IndexComponent, canActivate: [AuthGuard]}
-];
+import { UsersModule } from './users/users.module';
+import { SharedModule } from './modules/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
     NavbarComponent,
-    LoginDialogComponent,
-    RegisterPopupComponent
   ],
   imports: [
-    FormsModule,
-    ReactiveFormsModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    }),
-    RouterModule.forRoot(
-      appRoutes
-    ),
-    MatInputModule,
-    MatFormFieldModule,
-    MatCardModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatMenuModule,
-    MatDialogModule
-  ],
-  entryComponents: [
-    LoginDialogComponent,
-    RegisterPopupComponent
+    UsersModule,
+    SharedModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
---
+++ src/app/users/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { LoggedUserResponseDto } from './models/dtos/responses/logged-user.response.dto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  public currentUserSubject: BehaviorSubject<LoggedUserResponseDto>;
+  public currentUser: Observable<LoggedUserResponseDto>;
+
+  //Add user into storage and keep a reference
+  constructor(private router: Router) {
+    this.currentUserSubject = new BehaviorSubject<LoggedUserResponseDto>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue() {
+    return this.currentUserSubject.value;
+  }
+
+  //Set the user in the localStorage & currentUser
+  login(user) {
+    localStorage.setItem('currentUser', JSON.stringify(user));
+    this.currentUserSubject.next(user);
+  }
+
+  //Remove user from localStorage
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+    this.router.navigate(['']);
+  }
+}
