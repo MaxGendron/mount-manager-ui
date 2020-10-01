0c703fbb68a69db2e301c76e2fac5da887ad28f3
Maxime <gendron.maxime1@gmail.com>
2020-09-30 21:40:47 -0400

    My Mounts First pass

--- src/app/mounts/my-mounts/my-mounts.component.ts
+++ src/app/mounts/my-mounts/my-mounts.component.ts
@@ -1,4 +1,9 @@
+import { MountGenderEnum } from './../models/enum/mount-gender.enum';
+import { MountsService } from './../mounts.service';
 import { Component, OnInit } from '@angular/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
+import { MountResponseDto } from '../models/dtos/responses/mounts.response.dto';
 
 @Component({
   selector: 'app-my-mounts',
@@ -6,7 +11,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-mounts.component.scss'],
 })
 export class MyMountsComponent implements OnInit {
-  constructor() {}
+  private subscription: Subscription = new Subscription();
+  currentLang: string;
+  error: string;
+  mounts: MountResponseDto[] = new Array();
+  mountGenderEnum = MountGenderEnum;
+  
+  constructor(
+    private translateService: TranslateService,
+    private mountsService: MountsService
+  ) {
+    this.currentLang = translateService.currentLang;
+    translateService.onLangChange.subscribe((e : LangChangeEvent) => this.currentLang = e.lang)
+  }
 
-  ngOnInit(): void {}
+  async ngOnInit(): Promise<void> {
+    this.mounts = await this.mountsService.getMountForUserId().toPromise();
+  }
+
+  editMount(mountId: string): void {
+    //open popup for edit
+  }
+
+  addMount(): void {
+    //open popup for add
+  }
+
+  deleteMount(mountId: string): void {
+    this.subscription.add(this.mountsService.deleteMount(mountId).subscribe(() => {
+      //Remove mount from the lists
+      this.mounts = this.mounts.filter(m => m._id !== mountId);
+    }, () => {
+      this.error = this.translateService.instant('error.unexpected');
+    }));
+  }
 }
